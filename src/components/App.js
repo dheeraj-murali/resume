e020ae00f65db3091becf9793b243aba58cd78e4
Dheeraj <410dheeraj@gmail.com>
2019-11-11 20:58:23 +0530

    initial commit
    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Terminal from './Terminal';
+
+const App = () => {
+	return (
+		<div className='desktop'>
+			<Terminal />
+		</div>
+	);
+};
+
+export default App;
